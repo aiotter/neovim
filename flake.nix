e5ac3e081d84b40439ba7ff717828e42ab5274a1
aiotter <git@aiotter.com>
2022-09-30 00:31:33 +0900

    feat: Add overlay

--- flake.nix
+++ flake.nix
@@ -9,24 +9,27 @@
     # vim-plugins.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, vim-plugins }:
-    flake-utils.lib.eachSystem (builtins.attrNames vim-plugins.packages) (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        vimPlugins = pkgs.vimPlugins // vim-plugins.packages.${system};
-        callVimPlugin = pkgs.lib.callPackageWith (vimPlugins // pkgs);
-        allPlugins = map (fileName: callVimPlugin ./plugins/${fileName} { }) (builtins.attrNames (builtins.readDir ./plugins));
-        neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
-          # beforePlugins = ""; # <- no such config for neovim
-          customRC = builtins.readFile ./vimrc-append.vim;
-          plugins = allPlugins;
-        };
-        neovimConfigWrapped = neovimConfig // {
-          neovimRcContent = builtins.readFile ./vimrc-prepend.vim + neovimConfig.neovimRcContent;
-        };
-      in
-      rec {
-        packages.default = with pkgs; wrapNeovimUnstable neovim-unwrapped neovimConfigWrapped;
-      }
-    );
+  outputs = { self, nixpkgs, flake-utils, vim-plugins }: {
+    overlays.default = final: prev: {
+      neovim = self.packages.${prev.system}.default;
+    };
+  } // flake-utils.lib.eachSystem (builtins.attrNames vim-plugins.packages) (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      vimPlugins = pkgs.vimPlugins // vim-plugins.packages.${system};
+      callVimPlugin = pkgs.lib.callPackageWith (vimPlugins // pkgs);
+      allPlugins = map (fileName: callVimPlugin ./plugins/${fileName} { }) (builtins.attrNames (builtins.readDir ./plugins));
+      neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+        # beforePlugins = ""; # <- no such config for neovim
+        customRC = builtins.readFile ./vimrc-append.vim;
+        plugins = allPlugins;
+      };
+      neovimConfigWrapped = neovimConfig // {
+        neovimRcContent = builtins.readFile ./vimrc-prepend.vim + neovimConfig.neovimRcContent;
+      };
+    in
+    rec {
+      packages.default = with pkgs; wrapNeovimUnstable neovim-unwrapped neovimConfigWrapped;
+    }
+  );
 }
