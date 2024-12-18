5a14ad9028146495b02bbba7489ae926a3319166
aiotter <git@aiotter.com>
2024-12-18 15:36:39 +0900

    fix(lsp): buggy behaviour on tab key

--- plugins/nvim-cmp.nix
+++ plugins/nvim-cmp.nix
@@ -27,6 +27,13 @@
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.close(),
           -- ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<CR>"] = function(fallback)
+            if cmp.visible() and cmp.get_selected_entry() then
+              cmp.confirm({ select = false })
+            else
+              fallback()
+            end
+          end,
 
           ["<C-n>"] = cmp.mapping(function(fallback)
             if cmp.visible() and cmp.get_selected_entry() ~= nil then
@@ -57,10 +64,10 @@
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-              feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-              cmp.complete()
+            -- elseif vim.fn["vsnip#available"](1) == 1 then
+            --   feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            -- elseif has_words_before() then
+            --   cmp.complete()
             else
               fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
             end
@@ -69,8 +76,8 @@
           ["<S-Tab>"] = cmp.mapping(function()
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-              feedkey("<Plug>(vsnip-jump-prev)", "")
+            -- elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            --   feedkey("<Plug>(vsnip-jump-prev)", "")
             end
           end, { "i", "s" }),
 
