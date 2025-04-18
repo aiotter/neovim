224c557089c81dcb2dddb17a2f42a470442497e4
aiotter <git@aiotter.com>
2025-04-18 22:57:33 +0900

    feat(plugin/oil): modify config

--- plugins/oil-nvim.nix
+++ plugins/oil-nvim.nix
@@ -3,13 +3,17 @@
   config = ''
     lua <<EOF
       require("oil").setup({
+        keymaps = {
+          ["<S-CR>"] = { "actions.select", opts = { tab = true } },
+        },
         columns = {"icon", "permissions"},
         view_options = {
           show_hidden = true,
           is_hidden_file = function(name, bufnr)
             return name ~= ".." and vim.startswith(name, ".")
           end,
-        }
+        },
+        cleanup_delay_ms = 20000, -- https://github.com/stevearc/oil.nvim/issues/517
       })
     EOF
   '';
