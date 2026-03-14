a015493750378a80127825fd5c079e09a6e32d25
aiotter <git@aiotter.com>
2026-03-14 21:48:13 +0900

    feat(filetype): infer base filetypes for eex templates

---
+++ runtime/filetype.lua
@@ -0,0 +1,9 @@
+vim.filetype.add({
+  pattern = {
+    -- Infer the base filetype from the filename before the .eex suffix.
+    [".+%.eex"] = function(path)
+      local original_name = vim.fs.basename(path):gsub("%.eex$", "")
+      return vim.filetype.match({ filename = original_name }) or "eelixir"
+    end,
+  },
+})
