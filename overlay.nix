67ff4a5cbfb675a2d9ca62eafaff8fd4713a0ce5
aiotter <git@aiotter.com>
2026-04-25 17:56:26 +0900

    feat(lsp/nix): patch nixfmt to allow one-line list

--- overlay.nix
+++ overlay.nix
@@ -5,6 +5,16 @@ let
 in
 
 {
+  nixfmt = prev.nixfmt.overrideAttrs (old: {
+    patches = old.patches or [ ] ++ [
+      (final.fetchpatch {
+        name = "allow-single-line-list.patch";
+        url = "https://github.com/NixOS/nixfmt/pull/353.patch";
+        hash = "sha256-iE58XhEIKNVrFz8WHMsUYZ8NrGVBtmxAal4xLICu5kk=";
+      })
+    ];
+  });
+
   vimPlugins = prev.vimPlugins // {
     hmts-nvim = prev.vimPlugins.hmts-nvim.overrideAttrs (old: {
       patches = old.patches or [ ] ++ [
